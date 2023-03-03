608332329418b0c8eebe56cde7710aace8e06dcb
Michal Piechowiak <nilfenheim@gmail.com>
2023-03-03 20:39:39 +0100

    Revert "test: framer motion"
    This reverts commit 5cad6b21f5cc5e69d8f31468e1057b7e0fb4360b.

--- app/shared/PageTransition/PageTransition.tsx
+++ app/shared/PageTransition/PageTransition.tsx
@@ -23,11 +23,11 @@ export const PageTransition = ({ children }: Children) => {
             y: 40
         },
         out: {
-            opacity: 0,
-            y: 40,
-            transition: {
-                duration: .6
-            }
+            // opacity: 0,
+            // y: 40,
+            // transition: {
+            //     duration: .7
+            // }
         },
         in: {
             opacity: 1,
